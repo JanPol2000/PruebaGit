84d9e2b2c146b34b1bbc85537ade988ce52d7187
JanPol <stinktoignorance>
2019-09-22 11:06:35 -0500

    Mi primer commit

---
+++ Arreglos1.java
@@ -0,0 +1,40 @@
+/*
+Elabore un programa que lea en una lista, una serie de
+números enteros, calcule la media de los n números e indique
+cuantos de los números dados son iguales a la media. 
+*/
+
+import java.util.Scanner;
+
+public class Arreglos1{
+   public static void main(String args[]){
+      
+      Scanner entrada = new Scanner(System.in);
+      int n;
+      int suma = 0;
+      int cont = 0;
+      float prom;
+      
+      System.out.print("Tamaño de la lista: ");
+      n = entrada.nextInt();
+      int num[] = new int[n];
+      
+      for(int x = 0; x < n; x++){
+         System.out.printf("Ingresa un numero entero %d: ", x + 1);
+         num[x] = entrada.nextInt();
+      }
+      
+      for(int x = 0; x < n; x++)
+         suma += num[x];  
+         
+      prom = suma / (float)n;
+      System.out.printf("La media es: %.2f\n", prom);
+      
+      for(int x = 0; x < n; x++)
+         if(num[x] == prom)
+            cont++;
+      
+      System.out.printf("Existen %d numeros iguales a la media", cont);
+      
+  }
+}
